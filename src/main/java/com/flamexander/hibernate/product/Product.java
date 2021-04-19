cfdc8c4968b88457cfd2bab901c180e50cf5442b
Maestro <sergeyspopov@gmail.com>
2021-04-19 19:24:37 +0200

    HW5

---
+++ src/main/java/com/flamexander/hibernate/product/Product.java
@@ -0,0 +1,62 @@
+package com.flamexander.hibernate.product;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products_new")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private int cost;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Product() {
+    }
+
+    public Product(Long id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Product(String title, int cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product [id = %d, title = %s, cost = %d]", id, title, cost);
+    }
+}
+
